2ccc0355f6975a729a73b4b62c40ef3afc9c343c
theepika117 <112749921+theepika117@users.noreply.github.com>
2024-02-09 10:17:54 +0000

    added comments

--- code review/data collection/store_data.py
+++ code review/data collection/store_data.py
@@ -3,17 +3,17 @@ import json
 import math
 
 
-def store_data(symbols):
+def store_data(symbols):                                                                                                            #method accepts dictionary
     """Stores the price history for all available symbols"""
     price_history = dict()
     count = 0
     # Getting prices and storing in Dictionary
     for symbol in symbols:
-        symbol_name = symbol["symbol"]
-        prices = get_candles(symbol_name)
+        symbol_name = symbol["symbol"]                                                                                              #symbol name retrieved from pick instruments module is added
+        prices = get_candles(symbol_name)                                                                                           #fetches price list from candlesticks module
         count += 1
         if len(prices) > 0 and len(set([candle[4] for candle in prices["list"] if not math.isnan(candle[4])])) != 1:
-            price_history[symbol_name] = prices
+            price_history[symbol_name] = prices                                                                                     #stores the price data only if the symbol has diverse closing price
             print(f"{count} completed; {len(symbols) - count} remaining")
         else:
             with open("Available Instruments.txt", "a") as file:
@@ -26,3 +26,11 @@ def store_data(symbols):
             json.dump(price_history, file, indent=4)
         print("Price data saved successfully.")
     return
+
+
+
+
+
+    '''
+line 15 why is set() neccessary
+    '''
